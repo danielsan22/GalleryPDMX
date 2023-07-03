3cf17e08cce5ee35272b42c72cc4873ef00fdd91
Daniel SR <8375685+danielsan22@users.noreply.github.com>
2023-07-03 11:39:23 -0600

    Initial Commit

---
+++ GalleryPDMX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryPDMX
+//
+//  Created by Daniel Sanchez on 03/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
